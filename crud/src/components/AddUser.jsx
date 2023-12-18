1f12d4546be5ad94a8deb23cdf859a7ccc456d15
raje1966 <rajendra_kulkarni1@rediffmail.com>
2023-12-18 12:28:33 +0530

    Adding files to react-project to github

--- crud/src/components/AddUser.jsx
+++ crud/src/components/AddUser.jsx
@@ -1,9 +1,61 @@
+import { useState } from "react";
 
+import { FormControl, FormGroup, InputLabel, Input, Typography, Button, styled } from "@mui/material";
 
+import { addUser } from "../service/api";
+const Container = styled(FormGroup)`
+    width: 50%;
+    margin: 5% auto 0 auto;
+    & > div {
+        margin-top: 20px;
+    }
+`
 
+const initialValues = {
+    name: '',
+    username: '',
+    email: '',
+    phone: ''
+}
 const AddUser = () => {
+
+    const [user, setUser] = useState(initialValues);
+    const onValueChange = (e) => {
+        setUser({ ...user, [e.target.name]: e.target.value })
+        console.log(user)
+    }
+
+    const addUserDetails = async () => {
+       await addUser(user);
+
+    }
+
     return (
-        <p>Hello from AddUser</p>
+        <Container>
+            <Typography variant="h4">Add User</Typography>
+            <FormControl>
+                <InputLabel>Name</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="name"/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Username</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="username"/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Email</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="email"/>
+            </FormControl>
+            <FormControl>
+                <InputLabel>Phone</InputLabel>
+                <Input onChange={(e) => onValueChange(e)} name="phone"/>
+            </FormControl>
+            <FormControl>
+                <Button onClick={() => addUserDetails()}  variant="contained">Add User</Button>
+            </FormControl>
+        </Container>
+
+
+
     )
 
 }
--- crud/src/components/NavBar.jsx
+++ crud/src/components/NavBar.jsx
@@ -1,20 +1,24 @@
 import { AppBar, Toolbar, Typography, styled } from '@mui/material';
 
+import { NavLink } from 'react-router-dom';
+
 const Header = styled(AppBar)`
     background: #111111;
 `
 
-const Tabs = styled(Typography)`
+const Tabs = styled(NavLink)`
     font-size: 20px;
     margin-right: 20px;
+    color: inherit;
+    text-decoration: none;
 `
 const NavBar = () => {
     return (
          <Header position="static">
             <Toolbar>
-                <Tabs>Code for Interview</Tabs>
-                 <Tabs>All Users</Tabs>
-                 <Tabs>Add User</Tabs>
+                <Tabs to="/">Code for Interview</Tabs>
+                <Tabs to="/all">All Users</Tabs>
+                <Tabs to="/add">Add User</Tabs>
             </Toolbar>
         </Header>
     );
